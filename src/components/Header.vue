50aee44aec76c7148f8927b6b6ef394aa9571c05
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-10 16:15:11 +0100

    Installs axios + adds Main.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Header</div>
+  <header class="d-flex align-items-center">
+    <img
+      role="button"
+      id="spotify-logo"
+      src="../assets/img/Spotify.png"
+      alt="Spotify Logo"
+    />
+  </header>
 </template>
 
 <script>
@@ -8,5 +15,13 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+Header {
+  background-color: #2e3a46;
+  height: 85px;
+  #spotify-logo {
+    margin-left: 20px;
+    width: 50px;
+  }
+}
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div class="container wrapper">
+      <div class="music-card text-white"></div>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Main",
+  data() {
+    return {
+      musicCards: [],
+    };
+  },
+  //   methods: {},
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => console.log(res.data));
+  },
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  background-color: #1e2d3b;
+  height: calc(100vh - 85px);
+  .wrapper {
+    padding: 50px 120px;
+  }
+}
+
+.music-card {
+  background-color: rgba(#fff, 0.15);
+  width: 200px;
+}
+</style>
